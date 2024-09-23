a6714d4a49f58928ec481dacd3f166e2363c31de
mayashley <54293844+mayashley@users.noreply.github.com>
2024-09-23 11:47:27 -0400

    images and mobile responsive
    added images, corrected code for the js, mobile responsive, icons from font awesome cdn and bootstrap changes.

--- exercise.js
+++ exercise.js
@@ -1,1 +1,12 @@
+// Add any required JS here
+
+// Smooth scrolling for internal links
+$(document).ready(function () {
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 500);
+    });
+});
 
